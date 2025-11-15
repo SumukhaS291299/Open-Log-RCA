b014892f5e587d45d796b6c6bb701d822cfafa79
Sumukha S <66332035+SumukhaS291299@users.noreply.github.com>
2025-11-15 20:56:38 +0530

    Decided to use fastapi to ingest logs

---
+++ rca_ingest/ingest.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Request
+import uvicorn
+app = FastAPI()
+
+# UI on statistics
+@app.get("/")
+def root():
+    return {"message": "RCA Ingestion API is running"}
+
+# Ingest logs
+@app.post("/ingest/log")
+async def ingest_log(request: Request):
+    body = await request.json()
+    raw_log = body.get("message", "")
+
+    print("Received log:", raw_log)
+
+    return {
+        "status": "received",
+        "log": raw_log
+    }
+
+# uvicorn app:app --reload --host 0.0.0.0 --port 8000
+if __name__ == '__main__':
+    uvicorn.run("rca_ingest.ingest:app", host="0.0.0.0", port=8000, reload=True)
